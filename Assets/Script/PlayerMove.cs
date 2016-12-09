5e6d2a1dc421db6d4c512362e4859ddc4b35567b
guillaume volgyesi <guillaume volgyesi>
2016-12-09 23:46:53 +0100

    add move,jump,camera and hitbox

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+    private SpriteRenderer PlayerSprite;
+    public Rigidbody2D rb2d;
+    [SerializeField] float speed;
+    [SerializeField] float jump;
+
+    private void awake()
+    {
+        
+    }
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        PlayerSprite = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+        /////////////////////FLIP THE PLAYER SPRITE/////////////////////
+        if (PlayerSprite != null)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                PlayerSprite.flipX = true;
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                PlayerSprite.flipX = false;
+            }
+        }
+        ///////////////////////JUMP/////////////////////////////
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = rb2d.velocity + Vector2.up * jump;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb2d.velocity.y);
+    }
+
+}
