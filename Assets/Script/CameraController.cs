71f180803c770d8dc2fc37f1e8cafbadb6020791
guillaume volgyesi <guillaume volgyesi>
2016-12-15 15:27:56 +0100

    add better shoot, better jump and efficient code =)

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -12,7 +12,8 @@ public class CameraController : MonoBehaviour
     {
         
         offset = transform.position - player.transform.position;
-        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        
+
         
     }
 
--- Assets/Script/PlayerScript/PlayerMove.cs
+++ Assets/Script/PlayerScript/PlayerMove.cs
@@ -7,61 +7,98 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] Rigidbody2D rb2d;
     [SerializeField]float speed;
     [SerializeField]float jump;
-    private LayerMask GroundCheck;
-    private bool IsGrounded;
-    
-    
+    [SerializeField] GameObject GroundCheck;
+    [SerializeField] bool IsTurnedRight = true;
+    float horizontal = 0.0f;
+    public Transform BulletPrefab;
+    [SerializeField]float shootingRate = 0.25f;
+    public bool IsGrounded;
+
 
     private void awake()
-    {
-
-    }
-
-    void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         PlayerSprite = GetComponent<SpriteRenderer>();
         Cursor.visible = true;
+    }
+
+    void Start()
+    {
+
+       
 
     }
 
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
         /////////////////////FLIP THE PLAYER SPRITE/////////////////////
-        if (PlayerSprite != null)
+        if (horizontal > 0 && !IsTurnedRight)
         {
-            if (Input.GetKey(KeyCode.A))
-            {
-                PlayerSprite.flipX = true;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                PlayerSprite.flipX = false;
-            }
+            Flip();
+        }
+        else if (horizontal < 0 && IsTurnedRight)
+        {
+            Flip();
         }
         ///////////////////////JUMP/////////////////////////////
-        IsGrounded = Physics2D.Linecast(transform.position, GroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        IsGrounded = Physics2D.Linecast(transform.position, GroundCheck.transform.position, 1 << LayerMask.NameToLayer("Ground"));
 
-        if (Input.GetButtonDown("Jump")&& IsGrounded)
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)
         {
             rb2d.velocity = rb2d.velocity + Vector2.up * jump;
         }
+
         ///////////////////FIRE/////////////////////////////
-        bool shoot = Input.GetButton ("Fire1");
-        if (shoot)
-        {
-            WeaponScript weapon = GetComponent<WeaponScript>();
-            if (weapon != null)
-            {
-               
-                weapon.Attack(false);
-            }
-        }
+      
+        if (Input.GetMouseButtonDown(0))
+            Attack(false);
     }
-        void FixedUpdate()
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(speed *horizontal, rb2d.velocity.y);
+    }
+
+    public void Attack(bool isEnemy)
+    {
+        
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+
+        var shotTransform = Instantiate(BulletPrefab) as Transform;
+
+            
+        shotTransform.position = transform.position;
+
+            
+        ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
         {
-            rb2d.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb2d.velocity.y);
+            shot.isEnemyShot = isEnemy;
         }
 
+            
+        MoveShot move = shotTransform.gameObject.GetComponent<MoveShot>();
+        if (move != null)
+        {
+            move.Direction = direction.normalized;
+        }
+        
+    }
+
     
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        //m_SpriteRenderer.flipX = !m_SpriteRenderer.flipX;
+
+        IsTurnedRight = !IsTurnedRight;
+    }
+
 }
